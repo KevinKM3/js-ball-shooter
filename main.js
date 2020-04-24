d633c2f7e23c5cac83378af3c59485852c6ab8aa
Kevin Matthews <kevmatthews3@gmail.com>
2020-04-23 23:48:34 -0400

    circle-element

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+let points = 0;
+let enemies = [];
+let bullets = [];
 let mouse = {
   x: canvas.width / 2,
   y: canvas.height / 2,
@@ -9,6 +12,35 @@ let mouse = {
 addEventListener("mousemove", (event) => {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
-
-  console.log(mouse);
 });
+
+class Circle {
+  constructor(x, y, r, c) {
+    this.x = x;
+    this.y = y;
+    this.radius = r;
+    this.color = c;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+function start() {}
+
+function update() {
+  requestAnimationFrame(update);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+start();
+update();
